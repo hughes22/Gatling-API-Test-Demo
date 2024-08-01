dacde99a44a157e4746a51debdb5ffd62f922d1e
Albert Hughes <albert.hughes@amalitech.com>
2024-08-01 12:00:36 +0000

    second commit

--- GatlingScalaProject/src/test/scala/api/APIDemo.scala
+++ GatlingScalaProject/src/test/scala/api/APIDemo.scala
@@ -1,42 +1,42 @@
 package api
 
 import io.gatling.core.Predef._
-import  io.gatling.http.Predef._
+import io.gatling.http.Predef._
 
-class APIDemo extends Simulation{
+class APIDemo extends Simulation {
 
   //Create Section for protocol
   val httpProtocol = http.baseUrl("https://reqres.in")
 
   //Scenerio
-val getUser = scenario("Get API demo")
-  .exec(
-    http("Get Single User")
-  .get("/api/users/2")
-      .check(status.is(200))
-      .check(jsonPath
-      ("$.data.first_name").is("Janet"))
-      .check(jsonPath("$.support.text").is("To keep ReqRes free, contributions towards server costs are appreciated!"))
-).pause(2)
+  val getUser = scenario("Get API demo")
+    .exec(
+      http("Get Single User")
+        .get("/api/users/2")
+        .check(status.is(200))
+        .check(jsonPath
+        ("$.data.first_name").is("Janet"))
+        .check(jsonPath("$.support.text").is("To keep ReqRes free, contributions towards server costs are appreciated!"))
+    ).pause(2)
 
   val createUser = scenario("Post API demo")
     .exec(
       http("Create User")
-      .post("/api/users")
-      .asJson
+        .post("/api/users")
+        .asJson
         //fetching the user data fro the resource
-       // .body(RawFileBody("data/user.json"))
-      .body(StringBody(
-        """
-          |{
-          |    "name": "Peter",
-          |    "job": "leader"
-          |}
-          |""".stripMargin))
-          .asJson
-      .check(status.is(201))
-      .check( jsonPath("$.name").is("Peter"))
-  ).pause(2)
+        // .body(RawFileBody("data/user.json"))
+        .body(StringBody(
+          """
+            |{
+            |    "name": "Peter",
+            |    "job": "leader"
+            |}
+            |""".stripMargin))
+        .asJson
+        .check(status.is(201))
+        .check(jsonPath("$.name").is("Peter"))
+    ).pause(2)
 
   val updateUser = scenario("Put API demo").exec(
     http("update User")
@@ -57,12 +57,12 @@ val getUser = scenario("Get API demo")
         .delete("/api/users/2")
         .check(status.is(204))
 
-  )
-//Setup
-setUp(
-  getUser.inject(atOnceUsers(12)),
-  createUser.inject(atOnceUsers(7)),
-  updateUser.inject(atOnceUsers(5)),
-  deleteUser.inject(atOnceUsers(12))
-).protocols(httpProtocol)
+    )
+  //Setup
+  setUp(
+    getUser.inject(atOnceUsers(12)),
+    createUser.inject(atOnceUsers(7)),
+    updateUser.inject(atOnceUsers(5)),
+    deleteUser.inject(atOnceUsers(12))
+  ).protocols(httpProtocol)
 }
